f642b8a32014dab0ec275636401b1269ceaaf3b5
Serdar45000 <serdar45000@gmail.com>
2021-04-26 02:27:46 +0200

    POO - Basics | Part 6 : Static

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once 'Speedometer.php';
+
+
+echo '10 km is equal to ' . Speedometer::convertKmToMiles(10) . ' miles !! ';
+echo '6.21 miles is equal to ' .Speedometer::convertMilesToKm(6.21) . ' km !!';
